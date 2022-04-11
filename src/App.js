20e0901131c116b637caba21c475abab6a98b42a
manjeetsingh <manjeetsingh100001@yahoo.com>
2022-04-11 13:06:56 +0530

    unit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [score,setScore]=useState(76);
+  const [wicket,setWicket]=useState(50);
+  const [ball,setBall]=useState(2);
+  const handleScore=(value)=>{
+    //condition
+    setScore(score+value)
+    
+  }
+  
+  const handleWicket=(value)=>{
+    //condition
+    setWicket(wicket+value)
+  }
+  const handleball=(value)=>{
+    //condition
+    setBall(ball+value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>India:</h3>
+    <div className="banner">
+      <div>
+        Score:{score}
+        <h1 className="scoreCount">Add Score
+          
+           <button onClick={handleScore(1)}>Add 1</button>
+           <button onClick={handleScore(1)}>Add 4</button>
+           <button onClick={handleScore(1)}>Add 6</button> 
+           
+          
+        </h1>
+      </div>
+      <div>
+        Wicket:{wicket}
+        <h1 className="wicketCount">
+           <button onClick={handleWicket(1)}>Add 1</button>
+            </h1>
+      </div>
+      <div>
+        Over:{Math.floor(ball/6).ball%6}
+        <h1 className="overCount">
+          <button onClick={handleball(1)}>Add 1</button>
+       </h1>
+      </div>
     </div>
+<h1>{score>100?"India Won":""}</h1>
+    {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+  </div>
   );
 }
 
